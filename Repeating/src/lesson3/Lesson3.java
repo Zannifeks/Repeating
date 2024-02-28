853a9dbf228f58b393c3e574a7974d6cf1db686e
Shilinski Rodion <Shilinski.rodion@outlook.com>
2024-02-28 15:45:54 +0100

    Update

---
+++ Repeating/src/lesson3/Lesson3.java
@@ -0,0 +1,17 @@
+package lesson3;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        int n0 = 0;
+        int n1 = 1;
+        int n2 = 2;
+        int n3 = 3;
+        int n4 = 4;
+        int n5 = 5;
+        int n6 = 6;
+        int n7 = 7;
+        int n8 = 8;
+        int n9 = 9;
+        int sum = (n0 + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9)
+    }
+}
